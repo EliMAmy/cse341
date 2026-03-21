a16ca0063987d6033166beb4cfa9fa77cdab9e73
Rocio Apaza <rociomarapaza@gmail.com>
2026-03-20 19:48:24 -0500

    second commit

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const bodyParser = require("body-parser")
 const mongodb =  require("./data/database");
 const app = express();
 const Port = process.env.PORT || 3000;
 
+app.use(express.json());
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin","*");
+    res.setHeader(
+        "Acccess-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Z-Key" 
+    );
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    next();
+});
 
 app.use("/", require("./routes/index"));
+
 
 mongodb.initDb((error) =>{
     if(error){
